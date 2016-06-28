1a2ed4534b0f1fbe54aca4c3cdb439c9a763a02f
Mahmudur Rahman <Mahmudur Rahman>
2016-06-28 12:05:21 +0600

    Initial commit

---
+++ app/src/main/java/com/example/mrkolince/myapplicationzz/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mrkolince.myapplicationzz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
